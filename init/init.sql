f34ffed379546fd7b9a8a576786f56070c9b1929
蓝染 <lanran@lanrandeMacBook-Air.local>
2025-07-17 17:20:47 +0800

    feat:完成项目基础框架搭建

---
+++ init/init.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS `user`;
+-- Create database if it does not exist
+CREATE DATABASE IF NOT EXISTS use_center;
+
+-- Use the created database
+USE use_center;
+
+-- Create user table
+CREATE TABLE IF NOT EXISTS `user`
+(
+    `id`          BIGINT AUTO_INCREMENT COMMENT '主键ID',
+    `name`        VARCHAR(64) NOT NULL COMMENT '用户名',
+    `gender`      TINYINT  DEFAULT 0 COMMENT '性别(0未知 1男 2女)',
+    `phone`       VARCHAR(20) COMMENT '手机号',
+    `email`       VARCHAR(255) UNIQUE COMMENT '邮箱',
+    `address`     VARCHAR(255) COMMENT '地址',
+    `is_deleted`  TINYINT  DEFAULT 0 COMMENT '是否删除(0正常 1删除)',
+    `create_time` DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uk_name` (`name`),
+    UNIQUE KEY `uk_phone` (`phone`),
+    INDEX `idx_phone` (`phone`)
+) COMMENT ='用户表' ENGINE = InnoDB
+                    DEFAULT CHARSET = utf8mb4;
+
